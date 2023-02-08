f737fdfd6c8e69f5fd9121e4d540b940b785aa1f
GEEBR1D <ozodyoq@gmail.com>
2023-02-08 16:15:26 +0300

    n

---
+++ main.js
@@ -0,0 +1,5 @@
+let first = document.querySelector('.project .first')
+let second = document.querySelector('.banner .container .text .project .second')
+first.addEventListener('click' , () => {
+    first.classList.toggle('active');
+})
